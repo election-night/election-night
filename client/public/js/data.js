2d632f79cfd95ba7f9b221a8228085da9b4f0716
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-11-03 18:49:29 -0400

    Created an empty .ajax() method for the submit to create the campaign input. As well as created an easily copied empty .ajax() method for future uses. Filled in all the necessary html words.

--- client/public/js/data.js
+++ client/public/js/data.js
@@ -2,5 +2,35 @@
     'use strict';
 
     window.ns = window.ns || {};
+
+    // .ajax() method to link to button submit to create campaign
+
+      $('.createCampaign')
+       .on('submit', function createCampaign(event) {
+          event.preventDefault();
+          $.ajax({
+            url: '/campaigns',
+            method: 'POST' ,
+            dataType: 'json',
+            data: JSON.stringify({
+
+            }),
+            headers: {
+              
+            }
+
+          })
+        })
+        .done(function handleSuccess(data){
+          console.log('It worked', data);
+        })
+        .fail(function handleFailure(xhr){
+          console.log('It didn"t work', xhr);
+        })
+
+
+
+
+
     console.log('connected');
 }());
