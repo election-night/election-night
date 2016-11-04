cf1a4c1ebc41f5e98ca98a2a744406e32b64b9fe
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-11-04 12:00:52 -0400

    Implemented the ajax method for the create campaign submission.

--- client/public/js/data.js
+++ client/public/js/data.js
@@ -3,34 +3,63 @@
 
     window.ns = window.ns || {};
 
-    // .ajax() method to link to button submit to create campaign
 
-      $('.createCampaign')
-       .on('submit', function createCampaign(event) {
+    $('.candidate')
+        .on('submit', function createCandidate(event) {
+            event.preventDefault();
+            $.ajax({
+                    url: '/candidates',
+                    method: 'GET',
+                    dataType: 'json',
+                    data: JSON.stringify({
+                      // 'first name':,
+                      // 'last name':,
+                      // 'image_url':,
+                      // 'intelligence':,
+                      // 'charisma':,
+                      // 'willpower':
+                    }),
+                    headers: {
+                      'Content-Type': 'application/json'
+                    }
+                })
+                .done(function handleSuccess(data) {
+                    console.log('It worked', data);
+                })
+                // Include statements shown to the user that the data did not go through
+                // else there was a 500 server error
+                .fail(function handleFailure(xhr) {
+                    console.log('It didn"t work', xhr);
+                });
+
+
+        });
+
+      $('.create-campaign')
+        .on('submit', function createCampaign(event){
           event.preventDefault();
           $.ajax({
-            url: '/campaigns',
-            method: 'POST' ,
+            url:'/campaigns',
+            method: 'POST',
             dataType: 'json',
-            data: JSON.stringify({
-
-            }),
+            // data: JSON.stringify{
+            //   'candidates':
+            //   'start_date':
+            // },
             headers: {
-              
+              'Content-Type':'application/json'
             }
-
           })
-        })
-        .done(function handleSuccess(data){
-          console.log('It worked', data);
-        })
-        .fail(function handleFailure(xhr){
-          console.log('It didn"t work', xhr);
-        })
+          .done(function campaignSuccess(data){
+            console.log('It Worked', data);
+          })
+          .fail(function campaignFailure(xhr){
+            console.log('it failed', xhr);
+          })
+
+        });
 
 
+    console.log('Data file Connected');
 
-
-
-    console.log('connected');
 }());
