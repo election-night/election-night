72b21b05d63713cf099bec75d8889b0b6d462e32
Benjamin Mangelsen <bmangelsen@lmao.local>
2016-11-04 16:04:50 -0400

    Updated single employee test and added an outline for remainder of endpoints in app

--- server/lib/campaign.rb
+++ server/lib/campaign.rb
@@ -4,8 +4,10 @@ class Campaign < ActiveRecord::Base
   has_and_belongs_to_many :candidates
   # validates :candidate1, :candidate2, presence: true
 
-  # def pick_winner
-  #
-  # end
+  def pick_winner
+    candidate1 = candidates[0]
+    candidate2 = candidates[1]
+    self.winner_id = candidate1.id
+  end
 
 end
