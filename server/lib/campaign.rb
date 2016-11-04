94083c73c23fdde27acc302af03ebe991d7e14d7
Benjamin Mangelsen <bmangelsen@lmao.local>
2016-11-04 14:59:39 -0400

    Updated test files for campaign and candidate

--- server/lib/campaign.rb
+++ server/lib/campaign.rb
@@ -1,8 +1,11 @@
 require 'active_record'
 
 class Campaign < ActiveRecord::Base
-
   has_and_belongs_to_many :candidates
-  validates :candidate1, :candidate2, :start_date, presence: true
+  # validates :candidate1, :candidate2, presence: true
+
+  # def pick_winner
+  #
+  # end
 
 end
--- server/test/candidate_test.rb
+++ server/test/candidate_test.rb
@@ -10,39 +10,90 @@ class CandidateTest < Minitest::Test
     Candidate
   end
 
-  def test_can_create_candidate
-    assert Candidate.create!(
-      first_name: "Ben",
-      last_name: "Mangelsen",
-      image_url: "https://urlsample.com",
-      intelligence: 3,
-      charisma: 3,
-      willpoer: 4
-    )
+  def candidate_info
+    {first_name: "Ben",
+    last_name: "Mangelsen",
+    image_url: "https://urlsample.com",
+    intelligence: 3,
+    charisma: 3,
+    willpower: 4}
   end
 
-  validates :first_name, :last_name, :image_url, :intelligence, :charisma, :willpower,
-            presence: true
+  def test_can_create_candidate
+    assert Candidate.create!(candidate_info)
+  end
 
-  def test_name_must_be_present
+  def test_first_name_must_be_present
+    assert_raises do
+      Candidate.create!(
+        last_name: "Mangelsen",
+        image_url: "https://urlsample.com",
+        intelligence: 3,
+        charisma: 3,
+        willpower: 4
+      )
+    end
+  end
 
+  def test_last_name_must_be_present
+    assert_raises do
+      Candidate.create!(
+        first_name: "Ben",
+        image_url: "https://urlsample.com",
+        intelligence: 3,
+        charisma: 3,
+        willpower: 4
+      )
+    end
   end
 
   def test_url_must_be_present
-
+    assert_raises do
+      Candidate.create!(
+        first_name: "Ben",
+        last_name: "Mangelsen",
+        intelligence: 3,
+        charisma: 3,
+        willpower: 4
+      )
+    end
   end
 
-  def test_intelligence_must_be_present
 
+  def test_intelligence_must_be_present
+    assert_raises do
+      Candidate.create!(
+        first_name: "Ben",
+        last_name: "Mangelsen",
+        image_url: "https://urlsample.com",
+        charisma: 3,
+        willpower: 4
+      )
+    end
   end
 
   def test_charisma_must_be_present
-
+    assert_raises do
+      assert Candidate.create!(
+        first_name: "Ben",
+        last_name: "Mangelsen",
+        image_url: "https://urlsample.com",
+        intelligence: 3,
+        willpower: 4
+      )
+    end
   end
 
   def test_willpower_must_be_present
-
+    assert_raises do
+      assert Candidate.create!(
+        first_name: "Ben",
+        last_name: "Mangelsen",
+        image_url: "https://urlsample.com",
+        intelligence: 3,
+        charisma: 3
+      )
+    end
   end
-
 
 end
