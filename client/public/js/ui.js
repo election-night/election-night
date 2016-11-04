ebff3924929ad546ca2d6293177881ebd9cd6476
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-11-04 16:04:47 -0400

    Created a UI file for the link buttons.

--- client/public/js/ui.js
+++ client/public/js/ui.js
@@ -2,5 +2,29 @@
     'use strict';
 
     window.ns = window.ns || {};
+
+    window.ns.toggleVisible = function toggleVisible(htmlElement){
+      htmlElement.toggleClass('visible');
+    }
+
+    $('.create-candidates')
+        .on('click', function showCandidate(event) {
+            event.preventDefault();
+            window.ns.toggleVisible($('.create-a-candidate'));
+        });
+    $('.show-candidates')
+        .on('click', function seeCandidates(event) {
+            event.preventDefault();
+            console.log('it works');
+            $('.candidates').toggleClass('visible');
+        });
+    $('.show-campaigns')
+        .on('click', function showCampaign(event) {
+            event.preventDefault();
+            console.log('it works');
+            $('.all-campaigns').toggleClass('visible');
+        });
+
+
     console.log('UI file Connected');
 }());
