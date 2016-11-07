910a2da836bd6da7e66f81606bb2ce4f851a5d76
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-11-06 19:02:36 -0500

    added more ajax calls for the added endpoints.

--- client/public/js/data.js
+++ client/public/js/data.js
@@ -1,38 +1,26 @@
 (function() {
-    'use strict';
+        'use strict';
 
-    window.ns = window.ns || {};
-
-    function postCandidates(candidateInfo) {
-        var firstname = candidateInfo.firstName
-        var lastname = candidateInfo.lastName
-        var avatar = candidateInfo.image_url
-        var intelligence = candidateInfo.intelligence
-        var charisma = candidateInfo.charisma
-        var willpower = candidateInfo.willpower
-        console.log(candidateInfo);
-
-        window.ns.postCandidates = postCandidates;
+        window.ns = window.ns || {};
 
         $('.candidate')
             .on('submit', function createCandidate(event) {
                 event.preventDefault();
                 $.ajax({
-                    url: '/candidates',
-                    method: 'POST',
-                    dataType: 'json',
-                    data: JSON.stringify({
-                        first_name: candidateInfo.firstName,
-                        last_name: candidateInfo.lastName,
-                        image_url: candidateInfo.image_url,
-                        intelligence: candidateInfo.intelligence,
-                        charisma: candidateInfo.charisma,
-                        willpower: candidateInfo.willpower
-                    }),
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-
+                        url: '/candidates',
+                        method: 'POST',
+                        dataType: 'json',
+                        data: JSON.stringify({
+                            first_name: candidateInfo.firstName,
+                            last_name: candidateInfo.lastName,
+                            image_url: candidateInfo.image_url,
+                            intelligence: candidateInfo.intelligence,
+                            charisma: candidateInfo.charisma,
+                            willpower: candidateInfo.willpower
+                        }),
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
                     })
                     .done(function handleSuccess(data) {
                         console.log('It worked', data);
@@ -41,87 +29,99 @@
                     // else there was a 500 server error
                     .fail(function handleFailure(xhr) {
                         console.log('It didn"t work', xhr);
+                    });
+
+
+            });
+
+        $('.candidate')
+            .on('submit', function getCandidate(event) {
+                event.preventDefault();
+                $.ajax({
+                        url: '/candidates',
+                        method: 'GET',
+                        dataType: 'json',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
                     })
+                    .done(function handleYes(data) {
+                        console.log('success', data);
+                    })
+                    .fail(function handleNo(xhr) {
+                        console.log('failure', xhr)
+                    })
+            });
 
-              });
+        // GET method for candidateID
+        $( //'.')
+            .on('submit', function candidateId(event) {
+                event.preventDefault();
+                $.ajax({
+                        url: 'candidates/:id',
+                        method: 'GET',
+                        dataType: 'json',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    })
+                    .done(function candidateIdPass(data) {
+                        console.log('success', data)
+                    })
+                    .fail(function candidateIdFail(xhr) {
+                        console.log('failure', xhr);
+                    })
+            });
 
-        };
+            // Get Candidate Wins ajax
 
-
-            // PATCH SHOULD BE A SUBMIT ON ITS OWN BUTTON FOR THIS TO TAKE PLACE
-        // $('.candidate')
-        //     .on('submit', function updateCandidate(event) {
-        //         event.preventDefault();
-        //         $.ajax({
-        //             url: '/candidates/:id/first_name',
-        //             method: 'PATCH',
-        //             dataType: 'json',
-        //             data: JSON.stringify({
-        //                 first_name: candidateInfo.firstName
-        //             }),
-        //             headers: {
-        //                 'Content-Type': 'application/json'
-        //             }
-        //         })
-        //     });
-
-
-
-
-
-    // 
-    $('.candidate')
-        .on('submit', function recieveCandidate(event) {
-            event.preventDefault();
-            $.ajax({
-                    url: '/candidates',
-                    method: 'GET',
-                    dataType: 'json',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                })
-                .done(function handleSuccess(data) {
-                    console.log('It worked', data);
-                })
-                // Include statements shown to the user that the data did not go through
-                // else there was a 500 server error
-                .fail(function handleFailure(xhr) {
-                    console.log('It didn"t work', xhr);
+            $( //'.')
+                .on('submit', function candidateIdWins(event) {
+                    event.preventDefault();
+                    $.ajax({
+                        url: 'candidates/:id/wins',
+                        method: 'GET',
+                        data: 'json',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                        .done(function candidateIdWinsPass(data) {
+                            console.log('pass', data)
+                        })
+                        .fail(function candidateIdWinsFail(xhr) {
+                            console.log('failure', xhr)
+                        })
+                    })
                 });
 
 
-        });
 
+                $('.create-campaign')
+                .on('submit', function createCampaign(event) {
+                    event.preventDefault();
+                    $.ajax({
+                            url: '/campaigns',
+                            method: 'POST',
+                            dataType: 'json',
+                            // data: JSON.stringify{
+                            //   candidate.id:
+                            // },
+                            headers: {
+                                'Content-Type': 'application/json'
+                            }
+                        })
+                        .done(function campaignSuccess(data) {
+                            console.log('It Worked', data);
+                        })
+                        .fail(function campaignFailure(xhr) {
+                            console.log('it failed', xhr);
+                        })
 
-
-    $('.create-campaign')
-        .on('submit', function createCampaign(event) {
-            event.preventDefault();
-            $.ajax({
-                    url: '/campaign',
-                    method: 'POST',
-                    dataType: 'json',
-                    //data: JSON.stringify({
-                    // "candidate1":
-                    // "candidate2":
-                    //    })
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                })
-                .done(function campaignSuccess(data) {
-                    console.log('It Worked', data);
-                })
-                .fail(function campaignFailure(xhr) {
-                    console.log('it failed', xhr);
-                })
-
-        });
+                });
 
 
 
 
-    console.log('Data file Connected');
+                console.log('Data file Connected');
 
-}());
+            }());
